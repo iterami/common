8fa4589befbadd506295c47da3ffe69ba783b0a7
honzi <czechinthemail@gmail.com>
2016-04-17 02:26:22 -0700

    started audio.js, canvas.js, and settings.js

---
+++ js/canvas.js
@@ -0,0 +1,61 @@
+'use strict';
+
+function draw(){
+    buffer.clearRect(
+      0,
+      0,
+      width,
+      height
+    );
+
+    draw_logic();
+
+    canvas.clearRect(
+      0,
+      0,
+      width,
+      height
+    );
+    canvas.drawImage(
+      document.getElementById('buffer'),
+      0,
+      0
+    );
+
+    window.requestAnimationFrame(draw);
+}
+
+function init_canvas(){
+    resize();
+
+    window.requestAnimationFrame(draw);
+    window.setInterval(
+      logic,
+      30
+    );
+}
+
+function resize(){
+    height = window.innerHeight;
+    document.getElementById('buffer').height = height;
+    document.getElementById('canvas').height = height;
+    y = height / 2;
+
+    width = window.innerWidth;
+    document.getElementById('buffer').width = width;
+    document.getElementById('canvas').width = width;
+    x = width / 2;
+
+    resize_logic();
+}
+
+var buffer = document.getElementById('buffer').getContext('2d', {
+  'alpha': false,
+});
+var canvas = document.getElementById('canvas').getContext('2d', {
+  'alpha': false,
+});
+var height = 0;
+var width = 0;
+var x = 0;
+var y = 0;
