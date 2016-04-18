234399728fb3bd76e01f08ae0cd5216f4572a948
honzi <czechinthemail@gmail.com>
2016-04-17 18:28:18 -0700

    improved audio.js, started canvas-menu.js

--- js/audio.js
+++ js/audio.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function play_audio(id){
+    //if(settings['audio-volume'] <= 0){
+    //    return;
+    //}
+
+    document.getElementById(id).currentTime = 0;
+    document.getElementById(id).play();
+}
