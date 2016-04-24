4480c202cd652e0e577d8a46e7eda43f4b49b0f4
honzi <czechinthemail@gmail.com>
2016-04-24 12:10:17 -0700

    improved settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,14 +1,44 @@
 'use strict';
 
+function init_settings(newsettings, newprefix){
+    for(var setting in newsettings){
+        defaults[setting] = newsettings[setting]['default'];
+        settings[setting] = newsettings[setting]['value'];
+    }
+
+    prefix = newprefix;
+}
+
 function reset(){
     if(!window.confirm('Reset settings?')){
         return;
+    }
+
+    for(var setting in settings){
+        if(settings[setting] !== defaults[setting]){
+            settings[setting] = defaults[setting];
+        }
     }
 
     save();
 }
 
 function save(){
+    for(var setting in settings){
+        settings[setting] = document.getElementById(setting).value;
+
+        if(settings[setting] !== defaults[setting]){
+            window.localStorage.setItem(
+              prefix + setting,
+              settings[setting]
+            );
+
+        }else{
+            window.localStorage.removeItem(prefix + setting);
+        }
+    }
 }
 
+var defaults = {};
+var prefix = '';
 var settings = {};
