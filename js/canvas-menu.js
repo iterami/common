2de41453dffe9c8cd54baa19215491c53cf3338f
honzi <czechinthemail@gmail.com>
2016-04-23 23:32:25 -0700

    improved js/canvas-menu.js

--- js/canvas-menu.js
+++ js/canvas-menu.js
@@ -1,1 +1,102 @@
 'use strict';
+
+function draw(){
+    buffer.clearRect(
+      0,
+      0,
+      width,
+      height
+    );
+
+    draw_logic();
+
+    canvas.clearRect(
+      0,
+      0,
+      width,
+      height
+    );
+    canvas.drawImage(
+      document.getElementById('buffer'),
+      0,
+      0
+    );
+
+    window.requestAnimationFrame(draw);
+}
+
+function init_canvas(){
+    resize();
+    setmode(0);
+}
+
+function resize(){
+    if(mode <= 0){
+        return;
+    }
+
+    height = window.innerHeight;
+    document.getElementById('buffer').height = height;
+    document.getElementById('canvas').height = height;
+    y = height / 2;
+
+    width = window.innerWidth;
+    document.getElementById('buffer').width = width;
+    document.getElementById('canvas').width = width;
+    x = width / 2;
+
+    if(typeof resize_logic == 'function'){
+        resize_logic();
+    }
+}
+
+function setmode(newmode){
+    window.cancelAnimationFrame(animationFrame);
+    window.clearInterval(interval);
+
+    mode = newmode;
+
+    setmode_logic();
+
+    if(mode === 0){
+        // Main menu mode.
+        buffer = 0;
+        canvas = 0;
+
+    }else{
+        document.body.innerHTML =
+          '<canvas id=canvas></canvas><canvas id=buffer></canvas>';
+
+        var contextAttributes = {
+          'alpha': false,
+        };
+        buffer = document.getElementById('buffer').getContext(
+          '2d',
+          contextAttributes
+        );
+        canvas = document.getElementById('canvas').getContext(
+          '2d',
+          contextAttributes
+        );
+
+        resize();
+
+        animationFrame = window.requestAnimationFrame(draw);
+        interval = window.setInterval(
+          logic,
+          settings['ms-per-frame']
+        );
+    }
+}
+
+var animationFrame = 0;
+var buffer = 0;
+var canvas = 0;
+var height = 0;
+var interval = 0;
+var mode = 0;
+var width = 0;
+var x = 0;
+var y = 0;
+
+window.onresize = resize;
