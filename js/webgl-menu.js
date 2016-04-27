0047ab319ea0d12cad0a901f2b248689aee5411c
honzi <czechinthemail@gmail.com>
2016-04-26 20:56:43 -0700

    improved webgl.js and webgl-menu.js

--- js/webgl-menu.js
+++ js/webgl-menu.js
@@ -1,1 +1,87 @@
 'use strict';
+
+function draw(){
+    canvas.clearRect(
+      0,
+      0,
+      width,
+      height
+    );
+    canvas.drawImage(
+      document.getElementById('buffer'),
+      0,
+      0
+    );
+
+    animationFrame = window.requestAnimationFrame(draw);
+}
+
+function init_webgl(){
+    resize();
+    setmode(0);
+}
+
+function resize(){
+    if(mode <= 0){
+        return;
+    }
+
+    height = window.innerHeight;
+    document.getElementById('buffer').height = height;
+    document.getElementById('canvas').height = height;
+    y = height / 2;
+
+    width = window.innerWidth;
+    document.getElementById('buffer').width = width;
+    document.getElementById('canvas').width = width;
+    x = width / 2;
+
+    if(typeof resize_logic == 'function'){
+        resize_logic();
+    }
+}
+
+function setmode(newmode, newgame){
+    window.cancelAnimationFrame(animationFrame);
+    window.clearInterval(interval);
+
+    mode = newmode;
+    newgame = newgame || false;
+
+    setmode_logic(newgame);
+
+    if(mode === 0){
+        // Main menu mode.
+        buffer = 0;
+        canvas = 0;
+
+    }else{
+        if(newgame){
+            document.body.innerHTML =
+              '<canvas id=canvas></canvas><canvas id=buffer></canvas>';
+
+            buffer = document.getElementById('buffer').getContext('webgl');
+            canas = document.getElementById('canvas').getContext('webgl');
+
+            resize();
+        }
+
+        animationFrame = window.requestAnimationFrame(draw);
+        interval = window.setInterval(
+          logic,
+          settings['ms-per-frame']
+        );
+    }
+}
+
+var animationFrame = 0;
+var buffer = 0;
+var canvas = 0;
+var height = 0;
+var interval = 0;
+var mode = 0;
+var width = 0;
+var x = 0;
+var y = 0;
+
+window.onresize = resize;
--- js/webgl.js
+++ js/webgl.js
@@ -1,1 +1,52 @@
 'use strict';
+
+function draw(){
+    canvas.clearRect(
+      0,
+      0,
+      width,
+      height
+    );
+    canvas.drawImage(
+      document.getElementById('buffer'),
+      0,
+      0
+    );
+
+    window.requestAnimationFrame(draw);
+}
+
+function init_webgl(){
+    resize();
+
+    window.requestAnimationFrame(draw);
+    window.setInterval(
+      logic,
+      30
+    );
+}
+
+function resize(){
+    height = window.innerHeight;
+    document.getElementById('buffer').height = height;
+    document.getElementById('canvas').height = height;
+    y = height / 2;
+
+    width = window.innerWidth;
+    document.getElementById('buffer').width = width;
+    document.getElementById('canvas').width = width;
+    x = width / 2;
+
+    if(typeof resize_logic == 'function'){
+        resize_logic();
+    }
+}
+
+var buffer = 0;
+var canvas = 0;
+var height = 0;
+var width = 0;
+var x = 0;
+var y = 0;
+
+window.onresize = resize;
