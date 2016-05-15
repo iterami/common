ca9ed8f6b9fd76ba06db12ff18c2ee3c2a2f2719
honzi <czechinthemail@gmail.com>
2016-05-14 22:36:34 -0700

    started buttons.js

---
+++ js/buttons.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function settings_toggle(state){
+    state = state == void 0
+      ? document.getElementById('settings-button').value === '+'
+      : state;
+
+    if(state){
+        document.getElementById('settings').style.display = 'inline-block';
+        document.getElementById('settings-button').value = '-';
+
+    }else{
+        document.getElementById('settings').style.display = 'none';
+        document.getElementById('settings-button').value = '+';
+    }
+}
+
+var interval = 0;
