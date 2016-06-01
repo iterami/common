93798d6a5a0d592a2434c7e660380530c1f6fde4
honzi <czechinthemail@gmail.com>
2016-05-31 20:39:15 -0700

    added math functions from webgl.js to math.js

--- js/math.js
+++ js/math.js
@@ -1,1 +1,135 @@
 'use strict';
+
+function degrees_to_radians(degrees, decimals){
+    return round(
+      degrees * degree,
+      decimals
+    );
+
+function matrix_clone(id, newid){
+    matricies[newid] = matrix_create();
+    matrix_copy(
+      id,
+      newid
+    );
+}
+
+function matrix_copy(id, newid){
+    for(var key in matricies[id]){
+        matricies[newid][key] = matricies[id][key];
+    }
+}
+
+function matrix_create(){
+    return new Float32Array(16);
+}
+
+function matrix_identity(id){
+    for(var key in matricies[id]){
+        matricies[id][key] =
+          key % 5 === 0
+            ? 1
+            : 0;
+    }
+}
+
+function matrix_perspective(){
+    matricies['perspective'] = matrix_create();
+
+    matricies['perspective'][0] = .5;
+    matricies['perspective'][5] = 1;
+    matricies['perspective'][10] = -1;
+    matricies['perspective'][11] = -1;
+    matricies['perspective'][14] = -2;
+}
+
+function matrix_rotate(id, dimensions){
+    // Rotate X.
+    matrix_clone(
+      id,
+      'cache'
+    );
+    var cosine = Math.cos(dimensions[0]);
+    var sine = Math.sin(dimensions[0]);
+
+    matricies[id][4] = matricies['cache'][4] * cosine + matricies['cache'][8] * sine;
+    matricies[id][5] = matricies['cache'][5] * cosine + matricies['cache'][9] * sine;
+    matricies[id][6] = matricies['cache'][6] * cosine + matricies['cache'][10] * sine;
+    matricies[id][7] = matricies['cache'][7] * cosine + matricies['cache'][11] * sine;
+    matricies[id][8] = matricies['cache'][8] * cosine - matricies['cache'][4] * sine;
+    matricies[id][9] = matricies['cache'][9] * cosine - matricies['cache'][5] * sine;
+    matricies[id][10] = matricies['cache'][10] * cosine - matricies['cache'][6] * sine;
+    matricies[id][11] = matricies['cache'][11] * cosine - matricies['cache'][7] * sine;
+
+    // Rotate Y.
+    matrix_copy(
+      id,
+      'cache'
+    );
+    cosine = Math.cos(dimensions[1]);
+    sine = Math.sin(dimensions[1]);
+
+    matricies[id][0] = matricies['cache'][0] * cosine - matricies['cache'][8] * sine;
+    matricies[id][1] = matricies['cache'][1] * cosine - matricies['cache'][9] * sine;
+    matricies[id][2] = matricies['cache'][2] * cosine - matricies['cache'][10] * sine;
+    matricies[id][3] = matricies['cache'][3] * cosine - matricies['cache'][11] * sine;
+    matricies[id][8] = matricies['cache'][8] * cosine + matricies['cache'][0] * sine;
+    matricies[id][9] = matricies['cache'][9] * cosine + matricies['cache'][1] * sine;
+    matricies[id][10] = matricies['cache'][10] * cosine + matricies['cache'][2] * sine;
+    matricies[id][11] = matricies['cache'][11] * cosine + matricies['cache'][3] * sine;
+
+    // Rotate Z.
+    matrix_copy(
+      id,
+      'cache'
+    );
+    cosine = Math.cos(dimensions[2]);
+    sine = Math.sin(dimensions[2]);
+
+    matricies[id][0] = matricies['cache'][0] * cosine + matricies['cache'][4] * sine;
+    matricies[id][1] = matricies['cache'][1] * cosine + matricies['cache'][5] * sine;
+    matricies[id][2] = matricies['cache'][2] * cosine + matricies['cache'][6] * sine;
+    matricies[id][3] = matricies['cache'][3] * cosine + matricies['cache'][7] * sine;
+    matricies[id][4] = matricies['cache'][4] * cosine - matricies['cache'][0] * sine;
+    matricies[id][5] = matricies['cache'][5] * cosine - matricies['cache'][1] * sine;
+    matricies[id][6] = matricies['cache'][6] * cosine - matricies['cache'][2] * sine;
+    matricies[id][7] = matricies['cache'][7] * cosine - matricies['cache'][3] * sine;
+}
+
+function matrix_round(id, decimals){
+    for(var key in matricies[id]){
+        matricies[id][key] = round(
+          matricies[id][key],
+          decimals
+        );
+    }
+}
+
+function matrix_translate(id, dimensions){
+    matricies[id][12] -= matricies[id][0] * dimensions[0]
+      + matricies[id][4] * dimensions[1]
+      + matricies[id][8] * dimensions[2];
+    matricies[id][13] -= matricies[id][1] * dimensions[0]
+      + matricies[id][5] * dimensions[1]
+      + matricies[id][9] * dimensions[2];
+    matricies[id][14] -= matricies[id][2] * dimensions[0]
+      + matricies[id][6] * dimensions[1]
+      + matricies[id][10] * dimensions[2];
+   matricies[id][15] -= matricies[id][3] * dimensions[0]
+      + matricies[id][7] * dimensions[1]
+      + matricies[id][11] * dimensions[2];
+
+    matrix_round(id);
+}
+
+function round(number, decimals){
+    decimals = decimals || 7;
+
+    return Number(
+      Number.parseFloat(number + 'e+' + decimals)
+        + 'e-' + decimals
+    );
+}
+
+var degree = Math.PI / 180;
+var matricies = {};
