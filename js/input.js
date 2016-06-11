91a767d8641cda35d146b2b5db7be586ff8291ed
honzi <czechinthemail@gmail.com>
2016-06-10 23:44:58 -0700

    improved input.js

--- js/input.js
+++ js/input.js
@@ -8,44 +8,66 @@ function get_keycode(event){
     };
 }
 
-function onmousewheel(){
+function handle_keydown(event){
+    var key = get_keycode(event);
+}
+
+function handle_keyup(event){
+    var key = get_keycode(event);
+}
+
+function handle_mousedown(event){
+    mouse['down'] = true;
+    mouse['down-x'] = mouse['x'];
+    mouse['down-y'] = mouse['y'];
+}
+
+function handle_mousemove(event){
+    mouse['x'] = event.pageX;
+    mouse['y'] = event.pageY;
+}
+
+function handle_mouseup(event){
+    mouse['down'] = false;
+}
+
+function handle_mousewheel(event){
+    var delta = Number(
+      event.wheelDelta
+        || -event.detail
+    );
 }
 
 function init_input(){
+    keys = {};
+    mouse = {
+      'down': false,
+      'down-x': 0,
+      'down-y': 0,
+      'x': 0,
+      'y': 0,
+    };
+
     if('onmousewheel' in window){
-        window.onmousewheel = onmousewheel;
+        window.onmousewheel = handle_mousewheel;
 
     }else{
         document.addEventListener(
           'DOMMouseScroll',
-          onmousewheel
+          handle_mousewheel
           false
         );
     }
 }
 
-window.onkeydown = function(e){
-    var key = get_keycode(e);
-};
+var keys = {};
+var mouse = {};
 
-window.onkeyup = function(e){
-    var key = get_keycode(e);
-};
-
-window.onmousedown = function(e){
-};
-
-window.onmousemove = function(e){
-};
-
-window.onmouseup = function(e){
-};
-
-window.ontouchend = function(e){
-};
-
-window.ontouchmove = function(e){
-}
-
-window.ontouchstart = function(e){
-};
+window.onkeydown = handle_keydown;
+window.onkeyup = handle_keyup;
+window.onmousedown = handle_mousedown;
+window.onmousemove = handle_mousemove;
+window.onmouseup = handle_mouseup;
+window.ontouchend = handle_mouseup;
+window.ontouchmove = handle_mousemove;
+window.ontouchstart = handle_mousedown;
