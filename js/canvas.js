62b26d9fc9b8a3c104cf7fbb1f6dca5cef8c50b3
honzi <czechinthemail@gmail.com>
2016-06-12 22:42:30 -0700

    working on combining canvas.js and canvas-menu.js

--- js/canvas.js
+++ js/canvas.js
@@ -25,24 +25,19 @@ function draw(){
 
 function drawloop(){
     draw();
-    window.requestAnimationFrame(drawloop);
+    animationFrame = window.requestAnimationFrame(drawloop);
 }
 
 function init_canvas(){
-    buffer = document.getElementById('buffer').getContext('2d');
-    canvas = document.getElementById('canvas').getContext('2d');
     resize();
-
-    if(typeof logic === 'function'){
-        window.requestAnimationFrame(drawloop);
-        window.setInterval(
-          logic,
-          30
-        );
-    }
+    setmode(0);
 }
 
 function resize(){
+    if(mode <= 0){
+        return;
+    }
+
     height = window.innerHeight;
     document.getElementById('buffer').height = height;
     document.getElementById('canvas').height = height;
@@ -58,9 +53,62 @@ function resize(){
     }
 }
 
+function setmode(newmode, newgame){
+    window.cancelAnimationFrame(animationFrame);
+    window.clearInterval(interval);
+
+    mode = newmode;
+    newgame = newgame || false;
+    var msperframe = 0;
+
+    if(typeof setmode_logic === ' function'){
+        setmode_logic(newgame);
+
+    }else{
+        mode = 1;
+        newgame = true;
+        msperframe = 33;
+    }
+
+    if(mode === 0){
+        // Main menu mode.
+        buffer = 0;
+        canvas = 0;
+
+    }else{
+        if(newgame){
+            document.body.innerHTML =
+              '<canvas id=canvas></canvas><canvas id=buffer></canvas>';
+
+            buffer = document.getElementById('buffer').getContext('2d');
+            canvas = document.getElementById('canvas').getContext('2d');
+
+            resize();
+        }
+
+        if(typeof load_level === 'function'){
+            load_level(mode);
+        }
+
+        if(typeof draw_logic === 'function'){
+            animationFrame = window.requestAnimationFrame(drawloop);
+        }
+
+        if(typeof logic === 'function'){
+            interval = window.setInterval(
+              logic,
+              msperframe || settings['ms-per-frame']
+            );
+        }
+    }
+}
+
+var animationFrame = 0;
 var buffer = 0;
 var canvas = 0;
 var height = 0;
+var interval = 0;
+var mode = 0;
 var width = 0;
 var x = 0;
 var y = 0;
