b72b20540c12ba69ba57db7e2cb2a42ef9acc294
honzi <czechinthemail@gmail.com>
2016-07-10 16:41:26 -0700

    namespaced common input.js

--- js/input.js
+++ js/input.js
@@ -1,14 +1,6 @@
 'use strict';
 
-function get_keycode(event){
-    var code = event.keyCode || event.which;
-    return {
-      'code': code,
-      'key': String.fromCharCode(code),
-    };
-}
-
-function handle_event(event, object, key, todo, state){
+function input_handle_event(event, object, key, todo, state){
     if(object.hasOwnProperty(key)){
         if(object[key]['preventDefault']){
             event.preventDefault();
@@ -29,12 +21,12 @@ function handle_event(event, object, key, todo, state){
     return false;
 }
 
-function handle_keydown(event){
-    var key = get_keycode(event);
+function input_handle_keydown(event){
+    var key = input_keyinfo_get(event);
 
-    var solo = handle_event(
+    var solo = input_handle_event(
       event,
-      keys,
+      input_keys,
       key['code'],
       true,
       true
@@ -43,21 +35,21 @@ function handle_keydown(event){
         return;
     }
 
-    handle_event(
+    input_handle_event(
       event,
-      keys,
+      input_keys,
       'all',
       true,
       true
     );
 }
 
-function handle_keyup(event){
-    var key = get_keycode(event);
+function input_handle_keyup(event){
+    var key = input_keyinfo_get(event);
 
-    var solo = handle_event(
+    var solo = input_handle_event(
       event,
-      keys,
+      input_keys,
       key['code'],
       void 0,
       false
@@ -66,80 +58,80 @@ function handle_keyup(event){
         return;
     }
 
-    if(keys.hasOwnProperty('all')){
+    if(input_keys.hasOwnProperty('all')){
         var all = false;
-        for(var key in keys){
+        for(var key in input_keys){
             if(key !== 'all'
-              && keys[key]['state']){
+              && input_keys[key]['state']){
                 all = true;
                 break;
             }
         }
-        keys['all']['state'] = all;
+        input_keys['all']['state'] = all;
     }
 }
 
-function handle_mousedown(event){
-    mouse['down'] = true;
-    mouse['down-x'] = mouse['x'];
-    mouse['down-y'] = mouse['y'];
-    handle_event(
+function input_handle_mousedown(event){
+    input_mouse['down'] = true;
+    input_mouse['down-x'] = input_mouse['x'];
+    input_mouse['down-y'] = input_mouse['y'];
+    input_handle_event(
       event,
-      mouse['todo'],
+      input_mouse['todo'],
       'mousedown',
       true
     );
 }
 
-function handle_mousemove(event){
-    mouse['movement-x'] = event.movementX;
-    mouse['movement-y'] = event.movementY;
-    mouse['x'] = event.pageX;
-    mouse['y'] = event.pageY;
-    handle_event(
+function input_handle_mousemove(event){
+    input_mouse['movement-x'] = event.movementX;
+    input_mouse['movement-y'] = event.movementY;
+    input_mouse['x'] = event.pageX;
+    input_mouse['y'] = event.pageY;
+    input_handle_event(
       event,
-      mouse['todo'],
+      input_mouse['todo'],
       'mousemove',
       true
     );
 }
 
-function handle_mouseup(event){
-    mouse['down'] = false;
-    handle_event(
+function input_handle_mouseup(event){
+    input_mouse['down'] = false;
+    input_handle_event(
       event,
-      mouse['todo'],
+      input_mouse['todo'],
       'mouseup',
       true
     );
 }
 
-function handle_mousewheel(event){
+function input_handle_mousewheel(event){
     var delta = Number(
       event.wheelDelta
         || -event.detail
     );
-    handle_event(
+    input_handle_event(
       event,
-      mouse['todo'],
+      input_mouse['todo'],
       'mousewheel',
       true
     );
 }
 
-function init_input(keybinds, mousebinds){
+function input_init(keybinds, mousebinds){
     keybinds = keybinds || false;
     if(keybinds !== false){
-        update_keybinds(
+        input_keybinds_update(
           keybinds,
           true
         );
 
-        window.onkeydown = handle_keydown;
-        window.onkeyup = handle_keyup;
+        window.onkeydown = input_handle_keydown;
+        window.onkeyup = input_handle_keyup;
     }
 
-    mouse = {
+    input_mouse = {
       'down': false,
       'down-x': 0,
       'down-y': 0,
@@ -151,78 +143,86 @@ function init_input(keybinds, mousebinds){
     };
     mousebinds = mousebinds || false;
     if(mousebinds !== false){
-        update_mousebinds(
+        input_mousebinds_update(
           mousebinds,
           true
         );
 
-        window.onmousedown = handle_mousedown;
-        window.onmousemove = handle_mousemove;
-        window.onmouseup = handle_mouseup;
-        window.ontouchend = handle_mouseup;
-        window.ontouchmove = handle_mousemove;
-        window.ontouchstart = handle_mousedown;
+        window.onmousedown = input_handle_mousedown;
+        window.onmousemove = input_handle_mousemove;
+        window.onmouseup = input_handle_mouseup;
+        window.ontouchend = input_handle_mouseup;
+        window.ontouchmove = input_handle_mousemove;
+        window.ontouchstart = input_handle_mousedown;
 
         if('onmousewheel' in window){
-            window.onmousewheel = handle_mousewheel;
+            window.onmousewheel = input_handle_mousewheel;
 
         }else{
             document.addEventListener(
               'DOMMouseScroll',
-              handle_mousewheel,
+              input_handle_mousewheel,
               false
             );
         }
     }
 }
 
-function repeat_input_todos(){
-    for(var key in keys){
-        if(keys[key]['loop']
-          && keys[key]['state']){
-            keys[key]['todo']();
-        }
-    }
-    for(var mousebind in mouse['todo']){
-        if(mouse['todo'][mousebind]['loop']){
-            mouse['todo'][mousebind]['todo']();
-        }
-    }
-}
-
-function requestpointerlock(id){
-    document.getElementById(id).requestPointerLock();
-}
-
-function update_keybinds(keybinds, clear){
+function input_keybinds_update(keybinds, clear){
     clear = clear || false;
     if(clear){
-        keys = {};
+        input_keys = {};
     }
 
     for(var key in keybinds){
-        keys[key] = {};
-        keys[key]['loop'] = keybinds[key]['loop'] || false;
-        keys[key]['preventDefault'] = keybinds[key]['preventDefault'] || false;
-        keys[key]['solo'] = keybinds[key]['solo'] || false;
-        keys[key]['state'] = false;
-        keys[key]['todo'] = keybinds[key]['todo'] || function(){};
+        input_keys[key] = {};
+        input_keys[key]['loop'] = keybinds[key]['loop'] || false;
+        input_keys[key]['preventDefault'] = keybinds[key]['preventDefault'] || false;
+        input_keys[key]['solo'] = keybinds[key]['solo'] || false;
+        input_keys[key]['state'] = false;
+        input_keys[key]['todo'] = keybinds[key]['todo'] || function(){};
     }
 }
 
-function update_mousebinds(mousebinds, clear){
+function input_keyinfo_get(event){
+    var code = event.keyCode || event.which;
+    return {
+      'code': code,
+      'key': String.fromCharCode(code),
+    };
+}
+
+function input_mousebinds_update(mousebinds, clear){
     clear = clear || false;
     if(clear){
-        mouse['todo'] = {};
+        input_mouse['todo'] = {};
     }
 
     for(var mousebind in mousebinds){
-        mouse['todo'][mousebind] = {};
-        mouse['todo'][mousebind]['loop'] = mousebinds[mousebind]['loop'] || false;
-        mouse['todo'][mousebind]['preventDefault'] = mousebinds[mousebind]['preventDefault'] || false;
-        mouse['todo'][mousebind]['todo'] = mousebinds[mousebind]['todo'] || function(){};
+        input_mouse['todo'][mousebind] = {};
+        input_mouse['todo'][mousebind]['loop'] = mousebinds[mousebind]['loop'] || false;
+        input_mouse['todo'][mousebind]['preventDefault'] = mousebinds[mousebind]['preventDefault'] || false;
+        input_mouse['todo'][mousebind]['todo'] = mousebinds[mousebind]['todo'] || function(){};
     }
 }
 
-var keys = {};
-var mouse = {};
+function input_requestpointerlock(id){
+    document.getElementById(id).requestPointerLock();
+}
+
+function input_todos_repeat(){
+    for(var key in input_keys){
+        if(input_keys[key]['loop']
+          && input_keys[key]['state']){
+            input_keys[key]['todo']();
+        }
+    }
+    for(var mousebind in input_mouse['todo']){
+        if(input_mouse['todo'][mousebind]['loop']){
+            input_mouse['todo'][mousebind]['todo']();
+        }
+    }
+}
+
+var input_keys = {};
+var input_mouse = {};
