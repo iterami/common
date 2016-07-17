3ff44887d357076b0d0f82b48ab350bd0dab39e2
honzi <czechinthemail@gmail.com>
2016-07-17 01:33:19 -0700

    started common random.js

---
+++ js/random.js
@@ -0,0 +1,38 @@
+'use strict';
+
+function random_boolean(){
+    return random_integer(2) === 1;
+}
+
+function random_hex(){
+    var color = random_rgb();
+    var result = '';
+
+    var blue = '0' + color['blue'].toString(16);
+    var green = '0' + color['green'].toString(16);
+    var red = '0' + color['red'].toString(16);
+
+    result = red.slice(-2) + green.slice(-2) + blue.slice(-2);
+
+    return result;
+}
+
+function random_rgb(){
+  return {
+    'blue': random_integer(256),
+    'green': random_integer(256),
+    'red': random_integer(256),
+  };
+}
+
+function random_integer(max){
+    return Math.floor(Math.random() * max);
+}
+
+function random_string(length, characters){
+    var string = '';
+    for(var loopCounter = 0; loopCounter < length; loopCounter++){
+        string += characters[random_integer(characters.length - 1)];
+    }
+    return string;
+}
