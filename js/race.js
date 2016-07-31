91c69b4b52f31935e8e569bd3d8f88ad8dcee390
honzi <czechinthemail@gmail.com>
2016-07-31 12:45:36 -0700

    added race_racer_create(), fixed race variables

--- js/race.js
+++ js/race.js
@@ -1,5 +1,23 @@
 'use strict';
 
-var checkpoints = {};
-var racers = {};
-var walls = [];
+function race_racer_create(properties){
+    properties = properties || {};
+
+    properties['acceleration'] = properties['acceleration'] || .01;
+    properties['angle'] = properties['angle'] || 0;
+    properties['color'] = properties['color'] || '#fff';
+    properties['lap'] = properties['lap'] || 0;
+    properties['speed'] = properties['speed'] || 0;
+    properties['speed-max'] = properties['speed-max'] || 2;
+    properties['target'] = properties['target'] || 0;
+    properties['turn'] = properties['turn'] || .04;
+    properties['x'] = properties['x'] || 0;
+    properties['y'] = properties['y'] || 0;
+    properties['z'] = properties['z'] || 0;
+
+    race_racers.push(properties);
+}
+
+var race_checkpoints = [];
+var race_racers = [];
+var race_walls = [];
