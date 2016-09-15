7a7a6506864def96053779567d646d5c0f959af2
honzi <czechinthemail@gmail.com>
2016-09-14 22:00:25 -0700

    added separate object for oscillators

--- js/audio.js
+++ js/audio.js
@@ -2,15 +2,27 @@
 
 function audio_create(id, properties){
     audio_audio[id] = audio_audio[id] || {};
-    properties = properties || {};
-    audio_audio[id]['gain'] = audio_context.createGain();
-    audio_audio[id]['gain']['gain']['value'] = properties['volume'] || audio_volume;
-    audio_audio[id]['oscillator'] = audio_context.createOscillator();
-    audio_audio[id]['oscillator']['frequency']['value'] = properties['frequency'] || 1000;
-    audio_audio[id]['oscillator']['type'] = properties['type'] || 'sine';
+    for(var property in properties){
+        audio_audio[id][property] = properties[property];
+    }
+}
 
-    audio_audio[id]['oscillator'].connect(audio_audio[id]['gain']);
-    audio_audio[id]['gain'].connect(audio_context.destination);
+function audio_create_oscillator(id){
+    audio_oscillators[id] = {};
+    audio_oscillators[id]['duration'] = audio_audio[id]['duration'] || 0;
+    audio_oscillators[id]['gain'] = audio_context.createGain();
+    audio_oscillators[id]['gain']['gain']['value'] = audio_audio[id]['volume'] || audio_volume;
+    audio_oscillators[id]['oscillator'] = audio_context.createOscillator();
+    audio_oscillators[id]['oscillator']['frequency']['value'] = audio_audio[id]['frequency'] || 100;
+    audio_oscillators[id]['oscillator']['id'] = id;
+    audio_oscillators[id]['oscillator']['onended'] = function(){
+    audio_oscillators[id]['start'] = audio_audio[id]['start'] || 0;
+        audio_onended(this);
+    };
+    audio_oscillators[id]['oscillator']['type'] = audio_audio[id]['type'] || 'sine';
+
+    audio_oscillators[id]['oscillator'].connect(audio_oscillators[id]['gain']);
+    audio_oscillators[id]['gain'].connect(audio_context.destination);
 }
 
 function audio_init(default_volume){
@@ -20,32 +32,29 @@ function audio_init(default_volume){
       : 1;
 }
 
-function audio_set_volume(volume){
-    audio_audio[id]['gain']['gain']['value'] = volume;
-}
-
-function audio_set_volume_all(volume){
-    audio_volume = volume;
-
-    for(var id in audio_audio){
-        audio_set_volume(id, audio_volume);
-    }
+function audio_onended(that){
+    delete audio_oscillators[that.id];
 }
 
 function audio_start(id){
-    audio_audio[id]['oscillator'].start();
+    audio_create_oscillator(id);
+
+    audio_oscillators[id]['oscillator'].start(audio_oscillators[id]['start']);
+    audio_oscillators[id]['oscillator'].stop(audio_oscillators[id]['duration']);
 }
 
-function audio_stop(id){
-    audio_audio[id]['oscillator'].stop();
+function audio_stop(id, when){
+    audio_oscillators[id]['oscillator'].stop(when);
+    delete audio_oscillators[id];
 }
 
 function audio_stop_all(){
-    for(var id in audio_audio){
+    for(var id in audio_oscillators){
         audio_stop(id);
     }
 }
 
 var audio_audio = {};
 var audio_context = 0;
+var audio_oscillators = {};
 var audio_volume = 1;
