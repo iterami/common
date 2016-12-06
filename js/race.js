3dec3b23cf77c1c5a29c8b67622ddf374ad6106c
honzi <czechinthemail@gmail.com>
2016-12-06 15:13:43 -0800

    switched race.js to args, made default properties overwritable

--- js/race.js
+++ js/race.js
@@ -1,23 +1,30 @@
 'use strict';
 
-function race_racer_create(properties){
-    properties = properties || {};
+// Optional args: properties
+function race_racer_create(args){
+    args = args || {};
+    args['properties'] = args['properties'] || {};
 
-    properties['acceleration'] = properties['acceleration'] || .01;
-    properties['angle'] = properties['angle'] || 0;
-    properties['color'] = properties['color'] || '#fff';
-    properties['lap'] = properties['lap'] || 0;
-    properties['speed'] = properties['speed'] || 0;
-    properties['speed-max'] = properties['speed-max'] || 2;
-    properties['target'] = properties['target'] || 0;
-    properties['turn'] = properties['turn'] || .04;
-    properties['x'] = properties['x'] || 0;
-    properties['y'] = properties['y'] || 0;
-    properties['z'] = properties['z'] || 0;
+    for(var property in race_racer_default){
+        args['properties'][property] = args['properties'][property] || race_racer_default[property];
+    }
 
-    race_racers.push(properties);
+    race_racers.push(args['properties']);
 }
 
 var race_checkpoints = [];
+var race_racer_default = {
+  'acceleration': .01,
+  'angle': 0,
+  'color': '#fff',
+  'lap': 0,
+  'speed': 0,
+  'speed-max': 2,
+  'target': 0,
+  'turn': .04,
+  'x': 0,
+  'y': 0,
+  'z': 0,
+};
 var race_racers = [];
 var race_walls = [];
