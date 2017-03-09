de6111edf79450518ed0c5f8c19c75889366b63a
honzi <czechinthemail@gmail.com>
2017-03-09 02:13:21 -0800

    started common core.js

---
+++ js/core.js
@@ -0,0 +1,33 @@
+'use strict';
+
+// Required args: args, defaults
+function core_args(args){
+    if(args['args'] === void 0){
+        args['args'] = {};
+    }
+
+    for(var arg in args['defaults']){
+        if(args[arg] === void 0){
+            args[arg] = args['defaults'][arg];
+        }
+    }
+
+    return args['args'];
+}
+
+function core_uid_create(){
+    var uid = '';
+
+    for(var i = 0; i < 3; i++){
+        uid += core_uid_make();
+    }
+
+    return uid;
+}
+
+function core_uid_make(){
+    return parseInt(
+      Math.random() * 1e17,
+      10
+    ).toString(34);
+}
