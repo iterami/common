cbd46cad242f0fb644d762f344f04b60ac0f3367
honzi <czechinthemail@gmail.com>
2017-03-30 16:51:53 -0700

    fixed y-axis collisions

--- js/webgl.js
+++ js/webgl.js
@@ -656,8 +656,8 @@ function webgl_normals_collision(args){
     if(entity1['normals'][1] !== 0){
         if(entity1['normals'][1] === 1
           && entity0['dy'] < 0){
-            if(entity0['position']['x'] >= entity1['position']['x'] + entity1['vertices'][0] + 2
-              && entity0['position']['x'] <= entity1['position']['x'] + entity1['vertices'][3] - 2
+            if(entity0['position']['x'] <= entity1['position']['x'] + entity1['vertices'][0] + 2
+              && entity0['position']['x'] >= entity1['position']['x'] + entity1['vertices'][3] - 2
               && entity0['position']['y'] >= entity1['position']['y']
               && entity0['position']['y'] <= entity1['position']['y'] + 2
               && entity0['position']['z'] <= entity1['position']['z'] + entity1['vertices'][8] + 2
@@ -667,8 +667,8 @@ function webgl_normals_collision(args){
 
         }else if(entity1['normals'][1] === -1
           && entity0['dy'] > 0){
-            if(entity0['position']['x'] >= entity1['position']['x'] + entity1['vertices'][0] + 2
-              && entity0['position']['x'] <= entity1['position']['x'] + entity1['vertices'][3] - 2
+            if(entity0['position']['x'] <= entity1['position']['x'] + entity1['vertices'][0] + 2
+              && entity0['position']['x'] >= entity1['position']['x'] + entity1['vertices'][3] - 2
               && entity0['position']['y'] >= entity1['position']['y'] - 2
               && entity0['position']['y'] <= entity1['position']['y']
               && entity0['position']['z'] <= entity1['position']['z'] + entity1['vertices'][8] + 2
