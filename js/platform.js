69635a71ad5272a4191229b01bb5b548ae5a1678
honzi <czechinthemail@gmail.com>
2017-05-24 22:12:36 -0700

    added platform_player_reset() and platform_score_goal

--- js/platform.js
+++ js/platform.js
@@ -3,4 +3,31 @@
 function platform_init(){
 }
 
+// Required args: player
+// Optional args: all
+function platform_player_reset(args){
+    args = core_args({
+      'args': args,
+      'defaults': {
+        'all': false,
+      },
+    });
+
+    if(args['all']){
+        platform_players[args['player']] = {};
+    }
+
+    platform_players[args['player']] = core_handle_defaults({
+      'default': {
+        'coins': 0,
+        'lives': 1,
+        'x': 0,
+        'y': 0,
+        'y-velocity': 0,
+      },
+      'var': platform_players[args['player']],
+    });
+}
+
 var platform_players = {};
+var platform_score_goal = 1;
