2d824ed36aedf3148e3e1cdd74401e0627c8ea9f
honzi <czechinthemail@gmail.com>
2017-06-05 16:47:13 -0700

    added platform_jump() and player can-jump

--- js/platform.js
+++ js/platform.js
@@ -3,6 +3,19 @@
 function platform_init(){
 }
 
+// Optional args: id, velocity
+function platform_jump(args){
+    args = core_args({
+      'args': args,
+      'defaults': {
+        'id': 'player',
+        'velocity': 1,
+      },
+    });
+
+    platform_players['player']['y-velocity'] = args['velocity'];;
+}
+
 // Required args: player
 // Optional args: all
 function platform_player_reset(args){
@@ -19,6 +32,7 @@ function platform_player_reset(args){
 
     platform_players[args['player']] = core_handle_defaults({
       'default': {
+        'can-jump': false,
         'coins': 0,
         'lives': 1,
         'x': 0,
