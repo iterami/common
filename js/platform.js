9b59212fed92d3bec39e2ea20d42b99821c18170
honzi <czechinthemail@gmail.com>
2017-08-31 18:05:03 -0700

    made id arg optional

--- js/platform.js
+++ js/platform.js
@@ -35,21 +35,21 @@ function platform_jump(args){
     platform_players['player']['y-velocity'] = args['velocity'];;
 }
 
-// Required args: player
-// Optional args: all
+// Optional args: all, id
 function platform_player_reset(args){
     args = core_args({
       'args': args,
       'defaults': {
         'all': false,
+        'id': 'player',
       },
     });
 
     if(args['all']){
-        platform_players[args['player']] = {};
+        platform_players[args['id']] = {};
     }
 
-    platform_players[args['player']] = core_handle_defaults({
+    platform_players[args['id']] = core_handle_defaults({
       'default': {
         'can-jump': false,
         'coins': 0,
@@ -59,7 +59,7 @@ function platform_player_reset(args){
         'y': 0,
         'y-velocity': 0,
       },
-      'var': platform_players[args['player']],
+      'var': platform_players[args['id']],
     });
 }
 
