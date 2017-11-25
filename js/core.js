fb171b384e4fdf86726619d01983d65f29641bc7
Jan Hořava <czechinthemail@gmail.com>
2017-11-24 16:15:42 -0800

    combined sort.js into core.js

--- js/core.js
+++ js/core.js
@@ -1442,6 +1442,75 @@ function core_requestpointerlock(args){
     core_mouse['pointerlock-id'] = args['id'];
 }
 
+// Required args: array, todo
+// Optional args: reverse
+function core_sort_custom(args){
+    args = core_args({
+      'args': args,
+      'defaults': {
+        'reverse': false,
+      },
+    });
+
+    args['array'].sort(args['todo']);
+    if(args['reverse']){
+        args['array'].reverse();
+    }
+}
+
+// Required args: array
+// Optional args: reverse
+function core_sort_numbers(args){
+    core_sort_custom({
+      'array': args['array'],
+      'reverse': args['reverse'],
+      'todo': function(a, b){
+          return a - b;
+      },
+    });
+}
+
+// Required args: array
+function core_sort_random(args){
+    core_sort_custom({
+      'array': args['array'],
+      'todo': function(a, b){
+          return Math.random() - 0.5;
+      },
+    });
+}
+
+// Required args: array, property
+// Optional args: reverse
+function core_sort_property(args){
+    core_sort_custom({
+      'array': args['array'],
+      'reverse': args['reverse'],
+      'todo': function(a, b){
+          if(a[args['property']] > b[args['property']]){
+              return 1;
+
+          }else if(a[args['property']] < b[args['property']]){
+              return -1;
+          }
+
+          return 0;
+      },
+    });
+}
+
+// Required args, array
+// Optional args: reverse
+function core_sort_strings(args){
+    core_sort_custom({
+      'array': args['array'],
+      'reverse': args['reverse'],
+      'todo': function(a, b){
+          return a.localeCompare(b);
+      },
+    });
+}
+
 // Required args: storage
 // Optional args: prefix
 function core_storage_add(args){
