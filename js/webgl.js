6c7bf62882510228966df9d956995a36d7770582
Jan Hořava <czechinthemail@gmail.com>
2018-05-17 00:59:45 -0700

    added mouse-sensitivity usage to webgl_camera_first()

--- js/webgl.js
+++ js/webgl.js
@@ -85,8 +85,8 @@ function webgl_buffer_set_type(args){
 function webgl_camera_first(){
     if(core_mouse['pointerlock-state']){
         webgl_camera_rotate({
-          'x': core_mouse['movement-y'] / 10,
-          'y': core_mouse['movement-x'] / 10,
+          'x': (core_mouse['movement-y'] / 10) * core_storage_data['mouse-sensitivity'],
+          'y': (core_mouse['movement-x'] / 10) * core_storage_data['mouse-sensitivity'],
         });
     }
 }
