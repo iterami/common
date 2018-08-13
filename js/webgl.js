f9112c1f540312ce72b30e44bdb521fb3e6dd98d
Jan Hořava <czechinthemail@gmail.com>
2018-08-13 00:56:40 -0700

    fixed _me interfering with other character init

--- js/webgl.js
+++ js/webgl.js
@@ -1436,41 +1436,46 @@ function webgl_load_level_init(args){
         });
         webgl_character_homebase = [];
 
-    }else if(webgl_character_level() < 0
-      || args['character'] === 1){
-        if(args['json']['characters']
-          && args['json']['characters'] !== false){
-            for(let character in args['json']['characters']){
-                webgl_init_character({
-                  'camera-type': args['json']['characters'][character]['camera-type'],
-                  'camera-zoom-current': args['json']['characters'][character]['camera-zoom-current'],
-                  'camera-zoom-max': args['json']['characters'][character]['camera-zoom-max'],
-                  'collide-range': args['json']['characters'][character]['collide-range'],
-                  'collides': args['json']['characters'][character]['collides'],
-                  'dx': args['json']['characters'][character]['dx'],
-                  'dy': args['json']['characters'][character]['dy'],
-                  'dz': args['json']['characters'][character]['dz'],
-                  'entities': args['json']['characters'][character]['entities'],
-                  'experience': args['json']['characters'][character]['experience'],
-                  'health-current': args['json']['characters'][character]['health-current'],
-                  'health-max': args['json']['characters'][character]['health-max'],
-                  'id': args['json']['characters'][character]['id'],
-                  'inventory': args['json']['characters'][character]['inventory'],
-                  'jump-height': args['json']['characters'][character]['jump-height'],
-                  'level': args['json']['characters'][character]['level'],
-                  'speed': args['json']['characters'][character]['speed'],
-                });
+    }
 
-                if(args['json']['characters'][character]['id'] === webgl_character_id){
-                    webgl_character_homebase = args['json']['entities'];
-                }
+    if(args['json']['characters']
+      && args['json']['characters'] !== false){
+        for(let character in args['json']['characters']){
+            if(args['json']['characters'][character]['id'] === '_me'
+              && args['character'] !== 1
+              && webgl_character_level() > -1){
+                continue;
             }
 
-        }else if(!webgl_characters[webgl_character_id]){
             webgl_init_character({
-              'level': args['character'],
+              'camera-type': args['json']['characters'][character]['camera-type'],
+              'camera-zoom-current': args['json']['characters'][character]['camera-zoom-current'],
+              'camera-zoom-max': args['json']['characters'][character]['camera-zoom-max'],
+              'collide-range': args['json']['characters'][character]['collide-range'],
+              'collides': args['json']['characters'][character]['collides'],
+              'dx': args['json']['characters'][character]['dx'],
+              'dy': args['json']['characters'][character]['dy'],
+              'dz': args['json']['characters'][character]['dz'],
+              'entities': args['json']['characters'][character]['entities'],
+              'experience': args['json']['characters'][character]['experience'],
+              'health-current': args['json']['characters'][character]['health-current'],
+              'health-max': args['json']['characters'][character]['health-max'],
+              'id': args['json']['characters'][character]['id'],
+              'inventory': args['json']['characters'][character]['inventory'],
+              'jump-height': args['json']['characters'][character]['jump-height'],
+              'level': args['json']['characters'][character]['level'],
+              'speed': args['json']['characters'][character]['speed'],
             });
+
+            if(args['json']['characters'][character]['id'] === webgl_character_id){
+                webgl_character_homebase = args['json']['entities'];
+            }
         }
+
+    }else if(!webgl_characters[webgl_character_id]){
+        webgl_init_character({
+          'level': args['character'],
+        });
     }
 
     webgl_init({
