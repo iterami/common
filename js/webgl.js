ad941328c2fe62607829ea2045f8437f6c309e19
Jan Hořava <czechinthemail@gmail.com>
2018-08-17 14:30:21 -0700

    fixed -1 dx/dz reset

--- js/webgl.js
+++ js/webgl.js
@@ -1857,9 +1857,11 @@ function webgl_logicloop(){
     }
 
     if(webgl_character_level() === -1){
+        webgl_characters[webgl_character_id]['dx'] = 0;
         webgl_characters[webgl_character_id]['dy'] = 0;
-    }
-    if(webgl_characters[webgl_character_id]['jump-allow']){
+        webgl_characters[webgl_character_id]['dz'] = 0;
+
+    }else if(webgl_characters[webgl_character_id]['jump-allow']){
         webgl_characters[webgl_character_id]['dx'] = 0;
         webgl_characters[webgl_character_id]['dz'] = 0;
     }
