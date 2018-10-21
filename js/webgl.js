6bfe7b0dafb5c30cd380f9d623669e0887bdc57d
Jan Hořava <czechinthemail@gmail.com>
2018-10-21 11:02:31 -0700

    added missing path movement math

--- js/webgl.js
+++ js/webgl.js
@@ -2364,13 +2364,13 @@ function webgl_logicloop_handle_entity(entity){
             });
 
             core_entities[entity]['change']['translate-x'] = core_round({
-              'number': Math.cos(angle_xz),
+              'number': Math.cos(angle_xz) * Math.cos(angle_y),
             });
             core_entities[entity]['change']['translate-y'] = core_round({
               'number': Math.sin(angle_y),
             });
             core_entities[entity]['change']['translate-z'] = core_round({
-              'number': Math.sin(angle_xz),
+              'number': Math.sin(angle_xz) * Math.cos(angle_y),
             });
 
             if(core_entities[entity]['translate-x'] > point['translate-x']){
