44188e3a1132cf9c2ed91ec0ba60c474b4d92d46
Jan Hořava <czechinthemail@gmail.com>
2019-01-22 14:55:58 -0800

    expanded core_handle_mousedown() target check to include all buttons

--- js/core.js
+++ js/core.js
@@ -948,7 +948,7 @@ function core_handle_keyup(event){
 function core_handle_mousedown(event){
     if((core_menu_open
         && core_menu_block_events)
-      || event['target'].id === 'core-toggle'){
+      || event['target'].type === 'button'){
         return;
     }
 
