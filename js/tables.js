479ff6b35889a98d4cba3ee0d5dcc05042eabf58
Jan Hořava <czechinthemail@gmail.com>
2019-03-05 12:57:32 -0800

    added sort buttons and incomplete sort

--- js/tables.js
+++ js/tables.js
@@ -7,12 +7,28 @@ function tables_init(){
         let headers = Array.from(tables[i].firstElementChild.firstElementChild.children);
 
         for(let header in headers){
+            headers[header].innerHTML += '<input onclick="tables_sort(this,' + header + ',0)" type=button value=↑>'
+              + '<input onclick="tables_sort(this,' + header + ',1)" type=button value=↓>';
         }
     }
 }
 
-function tables_sort(button){
+function tables_sort(element, column, direction){
+    let table = element.closest('table');
+
     let column_content = [];
+    let rows = Array.from(table.firstElementChild.children);
+
+    for(let row in rows){
+        column_content.push(rows[row].children[column].innerHTML);
+    }
+
+    column_content.sort(function(a, b){
+      return a.localeCompare(b);
+    });
+
+    for(let row in rows){
+    }
 }
 
 tables_init();
