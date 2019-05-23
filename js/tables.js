24d47ce567a22079dd5953119982dc309bf5da4f
Jan Hořava <czechinthemail@gmail.com>
2019-05-23 00:53:42 -0700

    added main column class

--- js/tables.js
+++ js/tables.js
@@ -11,6 +11,10 @@ function tables_init(){
         let headers = Array.from(tables[i].firstElementChild.firstElementChild.children);
 
         for(let header in headers){
+            if(headers[header].classList.contains('main')){
+                tables_column_main = header;
+            }
+
             headers[header].innerHTML += '<input onclick="tables_sort(this,' + header + ',1)" type=button value=↑>'
               + '<input onclick="tables_sort(this,' + header + ',0)" type=button value=↓>';
         }
@@ -49,9 +53,9 @@ function tables_sort(element, column, direction){
             let parent = rows[row].children;
 
             if(parent[column].innerText === column_content[sorted]
-              && !used_rows.includes(parent[0].innerText)){
+              && !used_rows.includes(parent[tables_column_main].innerText)){
                 sorted_html += rows[row].outerHTML;
-                used_rows.push(parent[0].innerText);
+                used_rows.push(parent[tables_column_main].innerText);
 
                 break;
             }
@@ -60,5 +64,7 @@ function tables_sort(element, column, direction){
 
     table.firstElementChild.innerHTML = rows[0].outerHTML + sorted_html;
 }
+
+let tables_column_main = 0;
 
 tables_init();
