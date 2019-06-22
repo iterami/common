74287123d82109e709fff6c2761a956de8089131
Jan Hořava <czechinthemail@gmail.com>
2019-06-21 22:13:11 -0700

    created js/prefabs, moved uri.js to prefabs, split data.js into prefabs for canvas and webgl

--- js/prefabs/canvas.js
+++ js/prefabs/canvas.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Required args: id,
-function data_canvas_fence_2d(args){
+function prefabs_canvas_fence_2d(args){
     args = core_args({
       'args': args,
       'defaults': {
@@ -65,7 +65,7 @@ function data_canvas_fence_2d(args){
 }
 
 // Required args: id
-function data_canvas_tree_2d(args){
+function prefabs_canvas_tree_2d(args){
     args = core_args({
       'args': args,
       'defaults': {
@@ -126,77 +126,5 @@ function data_canvas_tree_2d(args){
       ],
       'x': args['x'],
       'y': args['y'] - args['height-base'],
-    });
-}
-
-// Required args: id,
-function data_webgl_tree_2d(args){
-    args = core_args({
-      'args': args,
-      'defaults': {
-        'billboard': false,
-        'color-base': [
-          .4, .2, 0, 1,
-          .4, .2, 0, 1,
-          .4, .2, 0, 1,
-        ],
-        'color-leaf': [
-          .1, .3, .1, 1,
-          .1, .3, .1, 1,
-          .1, .3, .1, 1,
-        ],
-        'dx': 0,
-        'dy': 0,
-        'dz': 0,
-        'x': 0,
-        'y': 0,
-        'z': 0,
-      },
-    });
-
-    core_entity_create({
-      'id': 'webgl-tree_' + args['id'] + '_base',
-      'properties': {
-        'billboard': args['billboard'],
-        'dx': args['dx'],
-        'dy': args['dy'],
-        'dz': args['dz'],
-        'translate-x': args['x'],
-        'translate-y': args['y'],
-        'translate-z': args['z'],
-        'vertex-colors': args['color-base'],
-        'vertices': [
-          1, 0, -.01, 1,
-          0, 4, -.01, 1,
-          -1, 0, -.01, 1,
-        ],
-      },
-    });
-    core_entity_create({
-      'id': 'webgl-tree_' + args['id'] + '_leaf',
-      'properties': {
-        'billboard': args['billboard'],
-        'draw-type': 'TRIANGLES',
-        'dx': args['dx'],
-        'dy': args['dy'],
-        'dz': args['dz'],
-        'translate-x': args['x'],
-        'translate-y': args['y'],
-        'translate-z': args['z'],
-        'vertex-colors': args['color-leaf'],
-        'vertices': [
-          3, 1, 0, 1,
-          0, 5, 0, 1,
-          -3, 1, 0, 1,
-        ],
-      },
-    });
-
-    core_group_add({
-      'entities': [
-        'webgl-tree_' + args['id'] + '_base',
-        'webgl-tree_' + args['id'] + '_leaf',
-      ],
-      'group': args['id'],
     });
 }
