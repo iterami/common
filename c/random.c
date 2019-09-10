c4a5f78d0c8d4951f4a359c738821771131463dd
Jan Hořava <czechinthemail@gmail.com>
2019-09-10 11:47:32 -0700

    added optional seed to random_seed()

--- c/random.c
+++ c/random.c
@@ -5,6 +5,11 @@ int random_integer(const int max){
     return rand() % max;
 }
 
-void random_seed(void){
-    srand(time(NULL));
+void random_seed(const int useseed, const int seed){
+    if(useseed == 1){
+        srand(seed);
+
+    }else{
+        srand(time(NULL));
+    }
 }
--- c/random.h
+++ c/random.h
@@ -1,4 +1,4 @@
 #pragma once
 
 int random_integer(const int max);
-void random_seed(void);
+void random_seed(const int useseed, const int seed);
