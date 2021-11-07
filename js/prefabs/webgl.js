c2b371bf4d168fa1f3d5a9caf28885d268c5d1a6
Jan Hořava <czechinthemail@gmail.com>
2021-11-06 21:10:49 -0700

    updated prefabs_webgl_tree_2d() arg namings and defaults

--- js/prefabs/webgl.js
+++ js/prefabs/webgl.js
@@ -858,12 +858,11 @@ function prefabs_webgl_tiles(args){
     }
 }
 
-// Required args: id,
 function prefabs_webgl_tree_2d(args){
     args = core_args({
       'args': args,
       'defaults': {
-        'billboard': false,
+        'billboard': 'y',
         'color-base': [
           .4, .2, 0, 1,
           .4, .2, 0, 1,
@@ -877,9 +876,10 @@ function prefabs_webgl_tree_2d(args){
         'dx': 0,
         'dy': 0,
         'dz': 0,
-        'x': 0,
-        'y': 0,
-        'z': 0,
+        'id': entity_id_count,
+        'translate-x': 0,
+        'translate-y': 0,
+        'translate-z': 0,
       },
     });
 
@@ -891,9 +891,9 @@ function prefabs_webgl_tree_2d(args){
           'dy': args['dy'],
           'dz': args['dz'],
           'id': 'webgl-tree_' + args['id'] + '_base',
-          'translate-x': args['x'],
-          'translate-y': args['y'],
-          'translate-z': args['z'],
+          'translate-x': args['translate-x'],
+          'translate-y': args['translate-y'],
+          'translate-z': args['translate-z'],
           'vertex-colors': args['color-base'],
           'vertices': [
             1, 0, -.01, 1,
@@ -908,9 +908,9 @@ function prefabs_webgl_tree_2d(args){
           'dy': args['dy'],
           'dz': args['dz'],
           'id': 'webgl-tree_' + args['id'] + '_leaf',
-          'translate-x': args['x'],
-          'translate-y': args['y'],
-          'translate-z': args['z'],
+          'translate-x': args['translate-x'],
+          'translate-y': args['translate-y'],
+          'translate-z': args['translate-z'],
           'vertex-colors': args['color-leaf'],
           'vertices': [
             3, 1, 0, 1,
