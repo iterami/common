b2f23980ab1e7b51a75df6e24b39f1abed430654
Jan Hořava <czechinthemail@gmail.com>
2023-10-06 19:23:41 -0700

    replaced strcmp() with strcasecmp() to sort case-insensitive

--- c/sort.c
+++ c/sort.c
@@ -9,9 +9,9 @@ int sort_compare_ints_desc(const void* a, const void* b){
 }
 
 int sort_compare_strings_asc(const void* a, const void* b){
-    return strcmp(*(const char **)a, *(const char **)b);
+    return strcasecmp(*(const char **)a, *(const char **)b);
 }
 
 int sort_compare_strings_desc(const void* a, const void* b){
-    return strcmp(*(const char **)b, *(const char **)a);
+    return strcasecmp(*(const char **)b, *(const char **)a);
 }
