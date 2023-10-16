f4e068247e238df8e0fc392e66ccaafc69a085a3
Jan Hořava <czechinthemail@gmail.com>
2023-10-15 17:49:21 -0700

    made top of tree base also .01 away

--- js/prefabs/webgl.js
+++ js/prefabs/webgl.js
@@ -642,7 +642,7 @@ function prefabs_webgl_tree_2d(args){
           'vertex-colors': args['base-color'],
           'vertices': [
             args['width-base'] / 2, 0, -.01,
-            0, height * .9, -.1,
+            0, height * .9, -.01,
             -args['width-base'] / 2, 0, -.01,
           ],
         },
