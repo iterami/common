ee6ed8eac09cf94b5f09d8b82f7fed1dd7e4faf9
Jan Hořava <czechinthemail@gmail.com>
2024-03-14 01:21:33 -0700

    set driver camera-rotate-y to vehicle rotate-y instead

--- js/webgl.js
+++ js/webgl.js
@@ -3645,7 +3645,7 @@ function webgl_vehicle_toggle(args){
             webgl_characters[args['id']]['change-rotate-' + axes[axis]] = 0;
             webgl_characters[args['id']]['change-translate-' + axes[axis]] = 0;
         }
-        webgl_characters[args['id']]['camera-rotate-y'] = webgl_characters[args['vehicle']]['camera-rotate-y'];
+        webgl_characters[args['id']]['camera-rotate-y'] = webgl_characters[args['vehicle']]['rotate-y'];
 
     }else{
         webgl_characters[args['id']]['vehicle'] = false;
