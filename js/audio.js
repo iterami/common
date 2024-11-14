48bfcdaa5df7209bdcde90ca7e20cc18d7bfa523
Jan Hořava <czechinthemail@gmail.com>
2024-11-13 17:01:08 -0800

    added audio_create() back in with boop defaults

--- js/audio.js
+++ js/audio.js
@@ -1,5 +1,16 @@
 'use strict';
 
+function audio_create(audios){
+    for(const audio in audios){
+        audio_audios[audio] = {
+          'duration': .1,
+          'frequency': 100,
+          'type': 'sine',
+          ...audios[audio],
+        };
+    }
+}
+
 function audio_start(id){
     const audio_context = new globalThis.AudioContext();
 
@@ -10,9 +21,14 @@ function audio_start(id){
     gain.connect(audio_context.destination);
 
     gain.gain.value = core_storage_data['audio-volume'];
-    oscillator.frequency.value = 100;
-    oscillator.type = 'sine';
+    oscillator.frequency.value = audio_audios[id]['frequency'];
+    oscillator.type = audio_audios[id]['type'];
 
     oscillator.start();
-    oscillator.stop(.1);
+    oscillator.stop(audio_audios[id]['duration']);
 }
+
+globalThis.audio_audios = {};
+audio_create({
+  'boop': true,
+});
