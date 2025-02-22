b53573aeee50a528a1f63d528e2753cf024a816b
Jan Hořava <czechinthemail@gmail.com>
2025-02-22 12:45:33 -0800

    removed entity unattach from dead characters

--- js/webgl.js
+++ js/webgl.js
@@ -3535,27 +3535,13 @@ function webgl_stat_modify(args){
                 }
 
                 if(args['target']['lives'] === 0){
+                    args['target']['gravity'] = 0;
+
                     const axes = 'xyz';
                     for(const axis in axes){
                         args['target']['change-rotate-' + axes[axis]] = 0;
                         args['target']['change-translate-' + axes[axis]] = 0;
                     }
-                    args['target']['gravity'] = 0;
-                    entity_group_modify({
-                      'groups': [
-                        'webgl_characters_' + args['target']['id'],
-                      ],
-                      'todo': function(entity){
-                           if(entity_groups['skybox'][entity]){
-                               return;
-                           }
-
-                           entity_entities[entity]['attach-to'] = false;
-                           for(const axis in axes){
-                               entity_entities[entity]['translate-' + axes[axis]] = args['target']['translate-' + axes[axis]];
-                           }
-                      },
-                    });
 
                 }else{
                     webgl_character_spawn(args['target']['id']);
