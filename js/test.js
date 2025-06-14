7ac8926c3827e49e120b441ed217e4c541309b7d
Jan Hořava <czechinthemail@gmail.com>
2025-06-14 03:47:40 -0700

    simplified property usage for test.js

--- js/test.js
+++ js/test.js
@@ -3,8 +3,8 @@
 // Required args: consts
 function test_consts(args){
     let results = '<tr class=header><td>Const<td>Value<td>Test';
-    for(const id in args['consts']){
-        const label = args['consts'][id];
+    for(const id in args.consts){
+        const label = args.consts[id];
         let result = false;
         let value = '';
         try{
@@ -22,25 +22,25 @@ function test_consts(args){
 // Required args: args, expect, function
 function test_function(args){
     let test = false;
-    const result = globalThis[args['function']](args['args']);
-    const type = core_type(args['expect']);
+    const result = globalThis[args.function](args.args);
+    const type = core_type(args.expect);
 
     if(type === 'function'){
-        test = args['expect'](result);
+        test = args.expect(result);
 
     }else if(type === 'array'
       || type === 'object'){
         test = true;
         for(const item in result){
-            if(args['expect'][item] === void 0
-              || result[item] !== args['expect'][item]){
+            if(args.expect[item] === void 0
+              || result[item] !== args.expect[item]){
                 test = false;
                 break;
             }
         }
 
     }else{
-        test = result === args['expect'];
+        test = result === args.expect;
     }
 
     return {
@@ -53,28 +53,28 @@ function test_function(args){
 function test_run(args){
     let results = '<tr class=header><td>Function<td>Args<td>Expected<td>Result<td>Test';
 
-    for(const test in args['tests']){
+    for(const test in args.tests){
         const test_args = {};
         Object.assign(
           test_args,
-          args['tests'][test]
+          args.tests[test]
         );
-        const args_type = core_type(args['tests'][test]['args']);
+        const args_type = core_type(args.tests[test].args);
         let args_json = '';
         if(args_type === 'object'){
-            test_args['args'] = {...args['tests'][test]['args']};
+            test_args.args = {...args.tests[test].args};
             const args_object = {};
-            for(const arg in test_args['args']){
-                if(core_type(test_args['args'][arg]) === 'function'){
+            for(const arg in test_args.args){
+                if(core_type(test_args.args[arg]) === 'function'){
                     args_object[arg] = core_replace({
                       'patterns': {
                         '\n': '<br>',
                       },
-                      'string': test_trim(test_args['args'][arg].toString()),
+                      'string': test_trim(test_args.args[arg].toString()),
                     });
                     continue;
                 }
-                args_object[arg] = test_args['args'][arg];
+                args_object[arg] = test_args.args[arg];
             }
             args_json = JSON.stringify(
               args_object,
@@ -83,36 +83,36 @@ function test_run(args){
             );
 
         }else if(args_type === 'function'){
-            args_json = test_trim(test_args['args'].toString());
+            args_json = test_trim(test_args.args.toString());
 
         }else{
-            test_args['args'] = args['tests'][test]['args'];
+            test_args.args = args.tests[test].args;
             args_json = JSON.stringify(
-              test_args['args'],
+              test_args.args,
               void 0,
               2
             );
         }
         const result = test_function(test_args);
-        const expect = core_type(args['tests'][test]['expect']) === 'function'
-          ? test_trim(args['tests'][test]['expect'].toString())
+        const expect = core_type(args.tests[test].expect) === 'function'
+          ? test_trim(args.tests[test].expect.toString())
           : JSON.stringify(
-            args['tests'][test]['expect'],
+            args.tests[test].expect,
             void 0,
             2
           );
         const result_json = JSON.stringify(
-          result['result'],
+          result.result,
           void 0,
           2
         );
 
-        results += '<tr ' + (!result['test'] ? ' style=background-color:#600' : '') + '>'
-          + '<td><a href=' + args['link'] + args['tests'][test]['function'] + '.htm>' + args['tests'][test]['function'] + '()</a>'
+        results += '<tr ' + (!result.test ? ' style=background-color:#600' : '') + '>'
+          + '<td><a href=' + args.link + args.tests[test].function + '.htm>' + args.tests[test].function + '()</a>'
           + '<td><pre>' + args_json
           + '</pre><td><pre>' + expect
           + '</pre><td><pre>' + result_json
-          + '</pre><td>' + result['test'];
+          + '</pre><td>' + result.test;
     }
 
     return results;
@@ -133,9 +133,9 @@ function test_time(args){
     let time_min = 0;
     let time_total = 0;
 
-    while(runs_done < args['runs']){
+    while(runs_done < args.runs){
         const time_before = globalThis.performance.now();
-        args['function'](args['function-args']);
+        args.function(args.function-args);
         const time_after = globalThis.performance.now();
 
         const diff = time_after - time_before;
@@ -152,10 +152,10 @@ function test_time(args){
     }
 
     return {
-      'average': time_total / args['runs'],
+      'average': time_total / args.runs,
       'max': time_max,
       'min': time_min,
-      'runs': args['runs'],
+      'runs': args.runs,
       'total': time_total,
     };
 }
